7d01ab89dcb2b491c2f8f5da31316f242481b52a
Arthurdw <mail@arthurdw.com>
2021-10-16 15:26:24 +0200

    :sparkles: Started on the base logic for webhook events

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 # Copyright Arthurdw 2021-present
 # Full MIT License can be found in `LICENSE` at the project root.
+
 from glob import glob
 from os import getenv
+from typing import Dict, Any
 
 from colorama import Style
 from dotenv import load_dotenv
@@ -15,6 +17,7 @@ class Ditto(Client):
         log.set_level(LogLevel.INFO)
         log.info("Instantiating Ditto client on Pincer version", __version__)
         Webserver()
+        Webserver.callbacks["webhook"] = self.webhook_handler
         self.load_cogs()
         super().__init__(token)
 
@@ -22,6 +25,9 @@ class Ditto(Client):
         """Load all cogs from the `cogs` directory."""
         for cog in glob("cogs/*.py"):
             self.load_cog(cog.replace("/", ".").replace("\\", ".")[:-3])
+
+    async def webhook_handler(self, hook: str, data: Dict[str, Any]):
+        print(hook, data)
 
     @Client.event
     async def on_ready(self):
--- utils/web.py
+++ utils/web.py
@@ -1,23 +1,34 @@
-from asyncio import ensure_future, get_event_loop
-from threading import Thread
+# Copyright Arthurdw 2021-present
+# Full MIT License can be found in `LICENSE` at the project root.
 
-from aiohttp.web import Application, Response, get, AppRunner, TCPSite
+from asyncio import ensure_future, get_event_loop
+from json import loads
+from threading import Thread
+from typing import Dict
+
+from aiohttp.web import Application, Response, post, AppRunner, TCPSite, Request
+from pincer.utils.types import Coro
 
 
 class Webserver:
+    callbacks: Dict[str, Coro] = {}
+
     def __init__(self):
         Thread(target=self.__run).run()
 
     @staticmethod
-    async def handle(request):
-        hook_id = request.match_info.get('hook', None)
-        print(hook_id)
-        return Response()
+    async def handle(request: Request) -> Response:
+        try:
+            await Webserver.callbacks["webhook"](request.match_info["id"], loads(await request.content.read()))
+        except Exception as e:
+            # TODO: proper response
+            raise e
+        return Response(body="Ok")
 
     @staticmethod
     def __get_runner():
         app = Application()
-        app.add_routes([get("/{hook}", Webserver.handle)])
+        app.add_routes([post("/webhook/{id}", Webserver.handle)])
         return AppRunner(app)
 
     def __run(self):
